f36b0b55b9eb1250e66422ab59db757aa6f50736
vcompanyb <vcompanyb@vulturdev.com>
2020-10-21 09:36:48 +0000

    output en common

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,8 +2,5 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-    mode: 'production',
-    output: {
-        publicPath: './'
-    }
+    mode: 'production'
 });
